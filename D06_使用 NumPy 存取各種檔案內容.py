03298ab5a0bb95cb45e31740db30546abd1dc55c
Simone Cheng <58110624+SimoneCheng@users.noreply.github.com>
2021-01-28 21:58:04 +0800

    Update D06_使用 NumPy 存取各種檔案內容.py

--- D06_使用 NumPy 存取各種檔案內容.py
+++ D06_使用 NumPy 存取各種檔案內容.py
@@ -8,13 +8,9 @@ import numpy as np
 #. 將下兩列array存成npz檔
 array1 = np.array(range(30))
 array2 = np.array([2,3,5])
-with open('multi_array.npz', 'wb') as f:
-    np.savez(f, array1=array1, array2=array2)
+np.savez('file1.npz', array1=array1, array2=array2)
 
 #2. 讀取剛剛的npz檔，加入下列array一起存成新的npz檔
-a = np.load('multi_array.npz')
-a.files
-print(a['array1'])
-print(a['array2'])
-with open('multi_array.npz', 'wb') as f:
-    np.savez(f, array1=array1, array2=array2, array3=array2)
+a = np.load('file1.npz')
+array3 = np.array([[4,5,6],[1,2,3]])
+np.savez('file2.npz', a['arr_0'], a['arr_1'], array3)
